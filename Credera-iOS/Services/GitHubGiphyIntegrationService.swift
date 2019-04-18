75a5fc9a3df02dc9827e59c74e28eca42e8c1e22
Craig Hatch <craig.hatch@credera.com>
2019-04-18 12:27:38 -0600

    handling promises in giphy api

--- Credera-iOS/Services/GitHubGiphyIntegrationService.swift
+++ Credera-iOS/Services/GitHubGiphyIntegrationService.swift
@@ -7,8 +7,41 @@
 //
 
 import Foundation
-class GitHubGiphyIntegrationService{
+import Promises
+class GitHubGiphyIntegrationService {
+    
+    let caller: RequestCaller
+    let giphyApi: GiphyApi
+    
+    init() {
+        self.caller = RequestCaller()
+        self.giphyApi = GiphyApi(caller: self.caller)
+    }
     
     
+    func getImagesForAllReposAndCommits(withGitHubData gitHubData: [GitHubData]) -> Promise<[GitHubAndGiphyData]>{
+        let promises = gitHubData.map { getImagesForEachCommit(withGitHubData: $0) }
+        return Promises.all(promises)
+            .then{ gitAndGiphyData in
+                return gitAndGiphyData
+        }
+    }
+    
+    
+    func getImagesForEachCommit(withGitHubData gitHubData: GitHubData) -> Promise<GitHubAndGiphyData>{
+        
+        let promises = gitHubData.commits.map { getImageForWord(withSearchWord: $0) }
+        return Promises.all(promises)
+            .then{ gitAndGiphyData in
+                return GitHubAndGiphyData(withUserName: gitHubData.userName, withRepoName: gitHubData.repoName, withCommmitGiphyDetails: gitAndGiphyData)
+        }
+    }
+    
+    
+    func getImageForWord(withSearchWord word: String) -> Promise<CommitGiphyDetails>{
+        return giphyApi.getImage(withSearchTerm: word).then{ response in
+            return CommitGiphyDetails(commit: word, imageLink: response.stuff!)
+        }
+    }
     
 }
